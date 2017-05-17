df849bebc933257d2de9d1534e0dd6a7ae6d2ce5
The_Learner <bsantosh909@gmail.com>
2017-05-17 18:51:24 +0545

    Update link.js

--- commands/fun/link.js
+++ commands/fun/link.js
@@ -1,11 +1,14 @@
 exports.run = (client, message, args) => {
 
+if(args[0] === "help"){
+message.channel.sendMessage("fr-forms : for link to fr-forms \n em-forms : for link to em-forms \ nfr-engine upgrade for link to FR battle engine upgrade");
+}else
 if(args[0] === "fr-forms"){
 message.channel.sendMessage("https://github.com/TheLearneer/FR-Overworld-Form-Change \nCredits __**The_Learner**__ and __**KDS**__");
 }else
 if(args[0] === "em-forms"){
 message.channel.sendMessage("https://github.com/KDSKardabox/Pokemon-Emerald-Overworld-Form-Change \nCredits __**KDS**__");
-}
+}else
 if(args[0] === "fr-engine upgrade"){
 message.channel.sendMessage("https://github.com/TheLearneer/FR-Engine-Upgrade\nCredits __**The_Learner#4692**__");
 }
